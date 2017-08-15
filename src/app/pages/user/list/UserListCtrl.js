850d9e320c394219392e5dcf6379502273fe72ee
”qinxiangyu“ <qinxiangyu@xinpianchang.com>
2017-08-15 19:59:52 +0800

    修改除root用户和本身可操作用户

--- src/app/pages/user/list/UserListCtrl.js
+++ src/app/pages/user/list/UserListCtrl.js
@@ -78,20 +78,27 @@
             console.log("btns : " + $scope.btns);
         };*/
 
-        $scope.currentUser = {};
-        //获取cookie中的用户id
-        //MANGER-USER-KEY=2-28cae871c5133305e1f77a723f5e98f6
+        $scope.currentUser= {};
         $scope.getCookie = function(){
             var name = "MANGER-USER-KEY";
-            var search = name + "=";
-            if (document.cookie.length > 0) {
-                var end = document.cookie.lastIndexOf("-")
-                $scope.currentUser = document.cookie.substring(search.length,end);
-                console.log("search.length : " + search.length);
-                console.log("end : " + end);
-            }
-            console.log("currentUser : " + $scope.currentUser);
+            var cookie_value =  $scope.readCookie(name);
+            var end = cookie_value.indexOf("-")
+            console.log("value : " + cookie_value);
+            $scope.currentUser = cookie_value.substring(0,end);
+            console.log("$scope.currentUser : " + $scope.currentUser);
         };
+
+        $scope.readCookie = function(name) {
+            var nameEQ = name + "=";
+            var ca = document.cookie.split(';');
+            for(var i=0;i < ca.length;i++) {
+                var c = ca[i];
+                while (c.charAt(0)==' ') c = c.substring(1,c.length);
+                if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+            }
+            return null;
+        }
+
         $scope.getCookie();
 
 
