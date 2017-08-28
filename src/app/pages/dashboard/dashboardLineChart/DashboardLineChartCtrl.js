2779d92589e989bbe9e85c0c6b5e320615f421f0
Wukunmeng <wukunmeng@xinpianchang.com>
2017-08-28 18:15:45 +0800

    modify line chart

--- src/app/pages/dashboard/dashboardLineChart/DashboardLineChartCtrl.js
+++ src/app/pages/dashboard/dashboardLineChart/DashboardLineChartCtrl.js
@@ -9,10 +9,10 @@
       .controller('DashboardLineChartCtrl', DashboardLineChartCtrl);
 
   /** @ngInject */
-  function DashboardLineChartCtrl(baConfig, layoutPaths, baUtil) {
+  function DashboardLineChartCtrl($http, toastr, baConfig, layoutPaths, baUtil) {
     var layoutColors = baConfig.colors;
     var graphColor = baConfig.theme.blur ? '#000000' : layoutColors.primary;
-    var chartData = [
+    /*var chartData = [
       { date: new Date(2012, 11), value: 0, value0: 0 },
       { date: new Date(2013, 0), value: 15000, value0: 19000},
       { date: new Date(2013, 1), value: 30000, value0: 20000},
@@ -42,85 +42,105 @@
       { date: new Date(2014, 11), value: 35300, value0: 13000},
       { date: new Date(2015, 0), value: 54800, value0: 13000},
       { date: new Date(2015, 1), value: 49800, value0: 13000}
-    ];
+    ];*/
 
-    var chart = AmCharts.makeChart('amchart', {
-      type: 'serial',
-      theme: 'blur',
-      marginTop: 15,
-      marginRight: 15,
-      dataProvider: chartData,
-      categoryField: 'date',
-      categoryAxis: {
-        parseDates: true,
-        gridAlpha: 0,
-        color: layoutColors.defaultText,
-        axisColor: layoutColors.defaultText
-      },
-      valueAxes: [
-        {
-          minVerticalGap: 50,
-          gridAlpha: 0,
-          color: layoutColors.defaultText,
-          axisColor: layoutColors.defaultText
-        }
-      ],
-      graphs: [
-        {
-          id: 'g0',
-          bullet: 'none',
-          useLineColorForBulletBorder: true,
-          lineColor: baUtil.hexToRGB(graphColor, 0.3),
-          lineThickness: 1,
-          negativeLineColor: layoutColors.danger,
-          type: 'smoothedLine',
-          valueField: 'value0',
-          fillAlphas: 1,
-          fillColorsField: 'lineColor'
-        },
-        {
-          id: 'g1',
-          bullet: 'none',
-          useLineColorForBulletBorder: true,
-          lineColor: baUtil.hexToRGB(graphColor, 0.5),
-          lineThickness: 1,
-          negativeLineColor: layoutColors.danger,
-          type: 'smoothedLine',
-          valueField: 'value',
-          fillAlphas: 1,
-          fillColorsField: 'lineColor'
-        }
-      ],
-      chartCursor: {
-        categoryBalloonDateFormat: 'MM YYYY',
-        categoryBalloonColor: '#4285F4',
-        categoryBalloonAlpha: 0.7,
-        cursorAlpha: 0,
-        valueLineEnabled: true,
-        valueLineBalloonEnabled: true,
-        valueLineAlpha: 0.5
-      },
-      dataDateFormat: 'MM YYYY',
-      export: {
-        enabled: true
-      },
-      creditsPosition: 'bottom-right',
-      zoomOutButton: {
-        backgroundColor: '#fff',
-        backgroundAlpha: 0
-      },
-      zoomOutText: '',
-      pathToImages: layoutPaths.images.amChart
-    });
-
-    function zoomChart() {
-      chart.zoomToDates(new Date(2013, 3), new Date(2014, 0));
+    function loadCurrentTrade() {
+        $http.get("/api/order/current").success(function(resp){
+            if(resp.success){
+                $scope.data = resp.data;
+                createChart();
+            } else {
+                toastr.error(resp.message)
+            }
+        });
     }
 
-    chart.addListener('rendered', zoomChart);
-    zoomChart();
-    if (chart.zoomChart) {
-      chart.zoomChart();
+    loadCurrentTrade();
+
+    function createChart(){
+
+        var chartData = [];
+        $scope.data.list.forEach(function(value,index,array){
+          chartData.push({date: new Date(value.date), value: value.total_order, value0: value.success_order})
+        });
+
+        var chart = AmCharts.makeChart('amchart', {
+          type: 'serial',
+          theme: 'blur',
+          marginTop: 15,
+          marginRight: 15,
+          dataProvider: chartData,
+          categoryField: 'date',
+          categoryAxis: {
+            parseDates: true,
+            gridAlpha: 0,
+            color: layoutColors.defaultText,
+            axisColor: layoutColors.defaultText
+          },
+          valueAxes: [
+            {
+              minVerticalGap: 50,
+              gridAlpha: 0,
+              color: layoutColors.defaultText,
+              axisColor: layoutColors.defaultText
+            }
+          ],
+          graphs: [
+            {
+              id: 'g0',
+              bullet: 'none',
+              useLineColorForBulletBorder: true,
+              lineColor: baUtil.hexToRGB(graphColor, 0.3),
+              lineThickness: 1,
+              negativeLineColor: layoutColors.danger,
+              type: 'smoothedLine',
+              valueField: 'value0',
+              fillAlphas: 1,
+              fillColorsField: 'lineColor'
+            },
+            {
+              id: 'g1',
+              bullet: 'none',
+              useLineColorForBulletBorder: true,
+              lineColor: baUtil.hexToRGB(graphColor, 0.5),
+              lineThickness: 1,
+              negativeLineColor: layoutColors.danger,
+              type: 'smoothedLine',
+              valueField: 'value',
+              fillAlphas: 1,
+              fillColorsField: 'lineColor'
+            }
+          ],
+          chartCursor: {
+            categoryBalloonDateFormat: 'JJ:NN',//MM YYYY
+            categoryBalloonColor: '#4285F4',
+            categoryBalloonAlpha: 0.7,
+            cursorAlpha: 0,
+            valueLineEnabled: true,
+            valueLineBalloonEnabled: true,
+            valueLineAlpha: 0.5
+          },
+          dataDateFormat: 'JJ:NN',
+          export: {
+            enabled: true
+          },
+          creditsPosition: 'bottom-right',
+          zoomOutButton: {
+            backgroundColor: '#fff',
+            backgroundAlpha: 0
+          },
+          zoomOutText: '',
+          pathToImages: layoutPaths.images.amChart
+        });
+        chart.addListener('rendered', zoomChart);
+        zoomChart();
+        if (chart.zoomChart) {
+            chart.zoomChart();
+        }
+    }
+
+    function zoomChart() {
+      chart.zoomToDates(new Date($scope.data.start), new Date($scope.data.end));
     }
   }
 })();
